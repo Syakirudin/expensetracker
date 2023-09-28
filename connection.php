3de89cd3ef733f1223a46b53da6480b59e23ae24
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2023-09-28 22:36:03 +0800

    connect to database

--- connection.php
+++ connection.php
@@ -1,32 +1,15 @@
 <?php
-// Database connection settings
-$hostname = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $database = "expensetracker";
 
-// Create a database connection
-$conn = new mysqli($hostname, $username, $password, $database);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-
-// Query to retrieve data from the 'users' table
-$sql = "SELECT id, name, email FROM users";
-$result = $conn->query($sql);
-
-// Create an associative array to store the data
-$data = array();
-while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-}
-
-// Close the database connection
-$conn->close();
-
-// Return data as JSON
-echo json_encode($data);
-
+echo "Connected successfully";
 ?>
--- dashboard.php
+++ dashboard.php
@@ -1,49 +1,48 @@
+<?php
+
+include 'connection.php';
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>User Table</title>
 </head>
 <body>
-    <h1>User Table</h1>
-    <table id="user-table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Data will be inserted here using JavaScript -->
-        </tbody>
-    </table>
+<form action="submit.php" method="post">
+        <!-- Text Input -->
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br>
 
-    <script>
-        // Function to fetch and populate data using AJAX
-        function fetchData() {
-            fetch('connection.php')
-                .then(response => response.json())
-                .then(data => {
-                    const tableBody = document.querySelector('#user-table tbody');
-                    tableBody.innerHTML = ''; // Clear previous data
+        <!-- Select Dropdown -->
+        <label for="gender">Gender:</label>
+        <select id="gender" name="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+        </select>
+        <br>
 
-                    data.forEach(row => {
-                        const newRow = document.createElement('tr');
-                        newRow.innerHTML = `
-                            <td>${row.id}</td>
-                            <td>${row.name}</td>
-                            <td>${row.email}</td>
-                        `;
-                        tableBody.appendChild(newRow);
-                    });
-                })
-                .catch(error => console.error('Error:', error));
-        }
+        <!-- Radio Button Group -->
+        <label>Choose an Option:</label>
+        <input type="radio" id="option1" name="option" value="option1">
+        <label for="option1">Option 1</label>
+        <br>
+        <label for="birthdate">Date of Birth:</label>
+        <input type="date" id="birthdate" name="birthdate" required>
+        <br>
+        
 
-        // Call fetchData() when the page loads
-        window.onload = fetchData;
-    </script>
+        <!-- Submit Button -->
+        <input type="submit" value="Submit">
+    </form>
+
 </body>
 </html>
